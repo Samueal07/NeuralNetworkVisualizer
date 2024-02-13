fd7ef9407785eb0683f64ffcb116c66fc44d03c4
Samueal Louis Dsouza <dsouzasamueal07@gmail.com>
2024-02-13 06:22:35 +0530

    add the neural network Vizualizer

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const canvas = document.getElementById("myCanvas");
-canvas.height = window.innerHeight;
-canvas.width = 200;
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 300;
 
 // getting a 2d context for making car
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 // Car Class and draw method in Car.js
 // const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
 const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
@@ -12,10 +15,10 @@ const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
 // const car = new Car(road.getLaneCenter(2), 100, 30, 50);
 
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
-car.draw(ctx);
+// car.draw(carCtx);
 
 animate();
-function animate() {
+function animate(time) {
   // calling it again and again so that it rerenders its position
   // prop dirlling till sensor.js main->car->sensor->castRays
   for (let i = 0; i < traffic.length; i++) {
@@ -23,18 +26,21 @@ function animate() {
   }
   car.update(road.borders, traffic);
 
-  canvas.height = window.innerHeight;
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
 
   // camera view for a moving car
-  ctx.save();
+  carCtx.save();
   // ctx.translate(0, -car.y);
-  ctx.translate(0, -car.y + canvas.height * 0.7);
-  road.draw(ctx);
+  carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+  road.draw(carCtx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx, "red");
+    traffic[i].draw(carCtx, "red");
   }
-  car.draw(ctx, "blue");
+  car.draw(carCtx, "blue");
 
-  ctx.restore();
+  carCtx.restore();
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, car.brain);
   requestAnimationFrame(animate);
 }
